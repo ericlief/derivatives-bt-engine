359c07e80563a1eb9dce9d611ecd8f37a7e560c1
Eric Lief <ericlief@git.com>
2026-06-23 16:44:30 -0400

    Local run script for tsmom

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tsmom --instruments 'MES,NQ,MCL,MZL,MZC,MZS,MZW,MGC,SIL' --max-contracts 1
+LOG_LEVEL=DEBUG tsmom --instruments 'MES,MNQ,MCL,MZL,MZC,MZS,MZW,MGC,SIL' --max-contracts 1 --client-id 15
 
